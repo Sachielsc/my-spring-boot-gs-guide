21a610fa7f7bff60d3a42f299c7ac377c6a95f16
Charles Shu <charles.shu@datacom.com>
2024-08-19 15:05:13 +1200

    add RestAssured

---
+++ initial/src/test/java/com/example/springboot/HelloControllerRestAssuredIntegrationTest.java
@@ -0,0 +1,32 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MyControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void setup() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    public void testGetEndpoint() {
+        given()
+            .contentType(ContentType.JSON)
+        .when()
+            .get("/")
+        .then()
+            .statusCode(200)
+            .body(equalTo("Greetings from Spring Boot!"));
+    }
+}
