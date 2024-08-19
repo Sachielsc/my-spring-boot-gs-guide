ff503493cd2c124781c0df4b52d37d1bda2a6823
Charles Shu <sachielsc@gmail.com>
2024-08-19 14:22:47 +1200

    roll back

--- complete/src/main/java/com/example/springboot/Application.java
+++ complete/src/main/java/com/example/springboot/Application.java
@@ -16,15 +16,16 @@ public class Application {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 
 			System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-			String[] beanNames = applicationContext.getBeanDefinitionNames();
-			Arrays.stream(beanNames)
-				//   .filter(beanName -> beanName.toLowerCase().contains("mvc"))
-				  .forEach(System.out::println);
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
 
 		};
 	}
